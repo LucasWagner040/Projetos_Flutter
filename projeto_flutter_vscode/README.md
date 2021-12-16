cb5ff72d02631e6df773fad2e9f57acad2e68d1a
LucasWagner040 <lucaswagner040@gmail.com>
2021-12-16 18:48:07 -0300

    Atualizações

--- projeto_flutter_vscode/README.md
+++ projeto_flutter_vscode/README.md
@@ -1,16 +1,14 @@
-# projeto_flutter_vscode
+# Contador
 
-A new Flutter project.
+## Esse é um projeto Inicial que estou fazendo atraves de um curso na Udemy.
 
-## Getting Started
+<h4 align="center"> 
+	🚧  Contador 🚀 Finalizado  🚧
+</h4>
 
-This project is a starting point for a Flutter application.
+### 🛠 Tecnologias
 
-A few resources to get you started if this is your first Flutter project:
+As seguintes ferramentas foram usadas na construção do projeto:
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- [Dart](https://dart.dev/)
+- [Flutter](https://flutter.dev/)
--- projeto_flutter_vscode/lib/main.dart
+++ projeto_flutter_vscode/lib/main.dart
@@ -1,87 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
   runApp(const MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({Key? key}) : super(key: key);
 
   @override
-
   Widget build(BuildContext context) {
-
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-
   const HomePage({Key? key}) : super(key: key);
 
   @override
-
-  _HomePageState createState() => _HomePageState();
-
+  HomePageState createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-
+class HomePageState extends State<HomePage> {
   int count = 0;
 
   void decrement() {
-
     setState(() {
       count--;
     });
-
-    print(count);
-
   }
 
   void increment() {
-
     setState(() {
       count++;
     });
-
-    print(count);
-
   }
 
   bool get isEmpty => count == 0;
-  bool get isFull => count == 20;
+  bool get isFull => count == 5;
 
   @override
-
   Widget build(BuildContext context) {
-
     return Scaffold(
-
-      backgroundColor: Colors.red,
-
+      backgroundColor: Colors.black87,
       body: Container(
-
         decoration: const BoxDecoration(
-
           image: DecorationImage(
-
-            image: AssetImage('essets/imagens/fundo_principal.jpg'),
+            image: AssetImage('essets/imagens/fundo-da-tela.jpg'),
             fit: BoxFit.cover,
           ),
         ),
-
         child: Column(
-          
           mainAxisAlignment: MainAxisAlignment.center,
-
           children: [
             Text(
               isFull ? 'Lotado' : 'Pode Entrar!',
@@ -91,19 +63,16 @@ class _HomePageState extends State<HomePage> {
                 fontWeight: FontWeight.w400,
               ),
             ),
-
             Padding(
               padding: const EdgeInsets.all(32),
               child: Text(
                 count.toString(),
                 style: TextStyle(
                   fontSize: 100,
-                  
                   color: isFull ? Colors.red : Colors.white,
-                  ),
+                ),
               ),
             ),
-
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -117,17 +86,14 @@ class _HomePageState extends State<HomePage> {
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20)),
                   ),
-
                   child: const Text(
                     'Saiu',
                     style: TextStyle(color: Colors.black, fontSize: 16),
                   ),
                 ),
-
                 const SizedBox(
                   width: 32,
                 ),
-
                 TextButton(
                   onPressed: isFull ? null : increment,
                   style: TextButton.styleFrom(
@@ -138,7 +104,6 @@ class _HomePageState extends State<HomePage> {
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20)),
                   ),
-                  
                   child: const Text(
                     'Entrou!',
                     style: TextStyle(color: Colors.black, fontSize: 16),
